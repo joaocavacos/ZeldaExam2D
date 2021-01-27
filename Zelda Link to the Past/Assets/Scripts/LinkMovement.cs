d6b2f2154aa823baa93cfa863292eefe75104310
João Cavacos <cavacosdoesgaming@gmail.com>
2021-01-27 04:51:31 +0000

    Player movement script and assets import
    Done with the basic movement and all assets are imported.
    Need to fix player sliding and constraints to the movement to be only 4 directions (no diagonal).

---
+++ Zelda Link to the Past/Assets/Scripts/LinkMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkMovement : MonoBehaviour
+{
+    public float speed;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    Vector2 movement;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+	void FixedUpdate()
+	{
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+            rb.MovePosition(rb.position + movement * (speed * 1.5f) * Time.fixedDeltaTime);
+		}
+		else
+		{
+            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        }
+        
+
+
+	}
+}
